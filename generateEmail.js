724b7ff365ec90c6ead2a8b17ea23f72880b4203
quthedeveloper <papafiobryan@gmail.com>
2025-10-25 20:38:21 +0000

    #

--- generateEmail.js
+++ generateEmail.js
@@ -25,8 +25,8 @@ userEmail.save();
 // set email up
 const transport =  nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASS
